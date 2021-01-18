373481e221266fa5d9e88d68d450a4a18ab481fc
lin <linhandev@qq.com>
2021-01-18 22:54:24 +0800

    ad

--- det/f_flg.py
+++ det/f_flg.py
@@ -1,12 +1,37 @@
 import os
 import argparse
 
+import paddlex as pdx
+
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("-i", "--input", type=str, help="图片路径")
 parser.add_argument("-o", "--output", type=str, help="结果xml路径")
 parser.add_argument("--bs", type=int, default=10, help="推理batchsize")
 parser.add_argument("--model", type=str,help="模型路径")
 args = parser.parse_args()
+
+
+def to_voc(name, xmin,ymin,xmax,ymax):
+    res = """<?xml version='1.0' encoding='UTF-8'?>
+    <annotation>
+      <filename>{}</filename>
+      <object_num>1</object_num>
+      <size>
+        <width>1920</width>
+        <height>1080</height>
+      </size>
+      <object>
+        <name>前起落架</name>
+        <bndbox>
+          <xmin>{}</xmin>
+          <ymin>{}</ymin>
+          <xmax>{}</xmax>
+          <ymax>{}</ymax>
+        </bndbox>
+      </object>
+    </annotation>""".format("1.png", 1,2,3,4)
+    return res
+
 
 model = pdx.load_model(args.model)
 transforms = transforms.Compose([
