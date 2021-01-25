036fb38876ed0b695993fb5adcd7ee30fd917016
lin <linhandev@qq.com>
2021-01-26 00:11:26 +0800

    sync

--- pdx/infer_gear.py
+++ pdx/infer_gear.py
@@ -1,9 +1,10 @@
 import os
+import os.path as osp
 
 import paddlex as pdx
 from paddlex.det import transforms
 
-model = pdx.load_model('output/yolov3/epoch_40')
+model = pdx.load_model('output/gear_clas/epoch_10')
 img_dir = "/home/aistudio/data/val"
 transforms = transforms.Compose([
     transforms.Resize(), transforms.Normalize()
@@ -11,8 +12,10 @@ transforms = transforms.Compose([
 
 names = []
 for n in os.listdir(img_dir):
-    if len(n) == 8:
-        result = model.batch_predict(nams, transforms=transforms)
-    names = []
-    print(result)
+    names.append(osp.join(img_dir, n))
+    # print(names)
+    if len(names) == 8:
+        result = model.batch_predict(names, transforms=transforms)
+        names = []
+        print(result)
 # pdx.det.visualize(image_name, result, threshold=0.001, save_dir='./output/plane_lg')
